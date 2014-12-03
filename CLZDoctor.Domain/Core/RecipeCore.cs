fbcec2ceac31da58a28bc87eb3475dc90d1fb0d0
zaishijieshang@163.com <ZS&sl5401>
2014-12-03 21:58:29 +0800

    郑胜添加的文件

---
+++ CLZDoctor.Domain/Core/RecipeCore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject;
+namespace CLZDoctor.Domain
+{
+    public class RecipeCore : IRecipeCore
+    {
+        private readonly static IKernel Kernel = new StandardKernel(new DomainModule());
+        public IEnumerable<Entities.Recipe> SelectList(int take, int skip,out int count)
+        {
+            count = Kernel.Get<IRecipeRepo>().Size();
+            return Kernel.Get<IRecipeRepo>().SelectList(take, skip);
+        }
+
+        public bool DeleteById(int Id)
+        {
+            return Kernel.Get<IRecipeRepo>().DeleteById(Id);
+        }
+
+    }
+}
---
+++ CLZDoctor.Domain/ICore/IRecipeCore.cs
@@ -0,0 +1,15 @@
+﻿using CLZDoctor.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CLZDoctor.Domain
+{
+    public interface IRecipeCore
+    {
+        IEnumerable<Recipe> SelectList(int take, int skip, out int count);
+
+        bool DeleteById(int Id);
+    }
+}
