b61c3586b7e8ba8cda5fc05a90867a73b7b58e14
zaishijieshang@163.com <ZS&sl5401>
2014-12-14 17:14:36 +0800

    添加接口

---
+++ CLZDoctor/Controllers/ApiControl/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CLZDoctor.Domain;
+using CLZDoctor.Entities;
+using CLZDoctor.Domain.Common;
+using Newtonsoft.Json;
+namespace CLZDoctor.Controllers.ApiControl
+{
+    public class AccountController : ApiController
+    {
+        private readonly IAccountCore _accountCore;
+
+        public AccountController(IAccountCore accountCore)
+        {
+            _accountCore = accountCore;
+        }
+        //IHttpActionResult
+        public string RegisterAccount(Account account)
+        {
+            if (string.IsNullOrEmpty(account.Name))
+                return Json(new OperationResult(OperationResultType.ParamError, "用户名不能为空！"));
+            if (string.IsNullOrEmpty(account.Mobile))
+                return Json(new OperationResult(OperationResultType.ParamError, "手机号不能为空！"));
+            if (string.IsNullOrEmpty(account.Password))
+                return Json(new OperationResult(OperationResultType.ParamError, "密码不能为空！"));
+
+            int re = _accountCore.CreateAccount(account);
+            if (re > 0)
+            {
+                return Json(new OperationResult(OperationResultType.Success, "用户创建成功！"));
+            }
+            else
+            {
+                return Json(new OperationResult(OperationResultType.Error, "用户创建失败！"));
+            }
+        }
+
+        public string UpdatePwd(int id, string oldpwd, string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                return Json(new OperationResult(OperationResultType.ParamError, "密码不能为空！"));
+
+            bool re = _accountCore.ModifyPassword(id, password);
+            if (re)
+                return Json(new OperationResult(OperationResultType.Success, "密码修改成功！"));
+            else
+                return Json(new OperationResult(OperationResultType.Error, "密码修改失败！"));
+        }
+
+        public string Json(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+
+    }
+}
---
+++ CLZDoctor/Controllers/ApiControl/FavoriteController.cs
@@ -0,0 +1,50 @@
+﻿using CLZDoctor.Domain;
+using CLZDoctor.Domain.Common;
+using CLZDoctor.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CLZDoctor.Controllers.ApiControl
+{
+    public class FavoriteController : ApiController
+    {
+        private readonly IFavoriteCore _favoriteCore;
+        public FavoriteController(IFavoriteCore favoriteCore)
+        {
+            _favoriteCore = favoriteCore;
+        }
+
+        public string AddFavorite(Favorite favorite)
+        {
+            if (favorite == null)
+                return Json(new OperationResult(OperationResultType.ParamError, "处方不能为空！"));
+            int re = _favoriteCore.CreateFavorite(favorite);
+            if (re > 0)
+                return Json(new OperationResult(OperationResultType.Success, "收藏成功！"));
+            else
+                return Json(new OperationResult(OperationResultType.Error, "收藏失败！"));
+        }
+
+        public string RomoveFavorite(int id)
+        {
+            if (string.IsNullOrEmpty(id.ToString()))
+                return Json(new OperationResult(OperationResultType.ParamError, "参数不能为空！"));
+            bool re = _favoriteCore.DeleteFavorites(id);
+            if (re)
+                return Json(new OperationResult(OperationResultType.Success, "删除收藏成功！"));
+            else
+                return Json(new OperationResult(OperationResultType.Error, "删除收藏失败！"));
+        }
+
+
+        public string Json(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
