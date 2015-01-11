5552a43215dc118e2bcf20ee6315499671e1928a
zmx <zhouming520_2009@126.com>
2015-01-11 10:17:18 +0800

    update

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> dd = new List<double> { 7, 6, 7, 5, 5, 6, 6, 7, 6 };
+
+            List<double> ddd = new List<double> { 5, 6, 6, 4, 5, 6, 6, 6, 4 };
+            for (int i = 0; i < dd.Count; i++)
+            {
+                Console.WriteLine((dd[i] - ddd[i]) / dd[i]);
+            }
+        }
+    }
+}
